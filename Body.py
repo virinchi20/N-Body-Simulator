1e9a9365dcb0e15d05541a35ed354bd9dd44df6e
virinchi20 <virinchilalwani20@gmail.com>
2025-03-02 16:47:15 -0500

    initial commit

---
+++ Body.py
@@ -0,0 +1,8 @@
+class Body:
+    
+    def __init__(self, position, velocity, mass):
+        self.position = position
+        self.velocity = velocity
+        self.mass = mass
+
+
---
+++ Simulation.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+import os
+import time
+from Body import Body
+
+
+
+def start():
+
+    pygame.init()
+
+    WIDTH, HEIGHT = 1000, 800
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+
+
+
+def main():
+
+    bodies = []
+
+
+    return
